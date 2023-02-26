76923b22195e9c48665a6f39b0a810896f6f22dc
Eliana <elianacristaldoo@gmail.com>
2023-02-25 21:35:17 -0300

    nginx

--- app.js
+++ app.js
@@ -1,33 +1,55 @@
 import routerLogin from "./router/routerLogin.js"
 import routerLogout from "./router/routerLogout.js"
 import routerSingup from "./router/routerSingup.js"
-/* import routerOut from "./router/routerOut.js"
+import routerOut from "./router/routerOut.js"
 import routerInfo from "./router/routerInfo.js"
 import routerNumberRandom from "./router/routerNumberRandom.js"
 import express from "express"
 import session from "express-session"
 import MongoStore from 'connect-mongo'
 import cookieParser from "cookie-parser"
+import cluster from 'cluster';
+import core from "os"
 
 const PORT = process.env.PORT || 8080
 const app = express()
 
-app.listen(PORT, ()=> console.log(`hola ` + PORT)) */
+/* app.listen(PORT, ()=> console.log(`hola ` + PORT)) */
 
-/* function ports(){
+//EJECUCIÓN
+//forever => forever start app.js FORK/(CLUSTER nofund)
+//           forever stop app.js
+//pm2 => pm2 start app.js = FORK
+//       pm2 start app.js -i número de instancias = CLUSTER
+//       pm2 delete app.js
+//       pm2 delete all app.js
+
+function ports(){
     let PORT = 8080
+    //nodemon app.js CLUSTER
     if(process.argv[2] === 'CLUSTER'){
-        
-    }else if(process.argv[2] === 'FORK'){
+        if(cluster.isMaster){
+            console.log(`Primary process ${process.pid}`)
+            for(let i=0; i<(core.cpus().length); i++){
+                cluster.fork();
+            }
+            cluster.on('exit', (worker, code) => {
+            console.log(`worker ${worker.process.pid} fué eliminado con ${code}`)
+            cluster.fork()
+         })
+    }//nodemon app.js FORK
+    }else if(process.argv[2] === undefined || process.argv[2] === 'FORK'){
+        //pm2 start app.js 1 proceso
+        //pm2 start app.js -i cant
+        app.listen(PORT, ()=> console.log(`Worker Process ${process.pid}`))
 
-    }else{
-        app.listen(PORT, ()=> console.log(`hola ` + PORT))
     }
 }
 
-ports() */
+ports() 
 
-/* app.use(express.urlencoded({extended:true}))
+
+app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(session({
@@ -43,33 +65,6 @@ app.use('/logout', routerLogout)
 app.use('/singup', routerSingup)
 app.use('/out', routerOut)
 app.use('/info', routerInfo)
-app.use('/api/randoms', routerNumberRandom) */
+app.use('/api/randoms', routerNumberRandom)
 
-/* import express from "express"
 
-const app = express()
-
-app.listen(8081, () => console.log(`Worker Process ${process.pid}`))
-
-app.get('/Holis', (req, res) => {
-    res.send(`Hols Mundo`)
-}) */
-
-import express from "express"
-import cluster from 'cluster'
-import core from "os"
-
-const app = express()
-
-if(cluster.isPrimary){
-console.log(`Primary process ${process.pid}`)
-for(let i=0; i<(core.cpus().length); i++){
-        cluster.fork()
-    }
-    cluster.on('exit', (worker, code) => {
-        console.log(`worker ${worker.process.pid} fué eliminado con ${code}`)
-        cluster.fork()
-    })
-}else{
-    app.listen(8080, () => console.log(`Worker Process ${process.pid}`))
-}
